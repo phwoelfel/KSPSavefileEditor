da8886aa95fec4535753bb7e489c9f914afd0098
Philip Woelfel <philip@woelfel.at>
2014-06-01 20:57:45 +0200

    added entry editor

---
+++ src/at/woelfel/philip/kspsavefileeditor/gui/EntryEditor.java
@@ -0,0 +1,149 @@
+package at.woelfel.philip.kspsavefileeditor.gui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+import at.woelfel.philip.kspsavefileeditor.backend.Entry;
+import at.woelfel.philip.kspsavefileeditor.backend.ChangeListener;
+import at.woelfel.philip.kspsavefileeditor.backend.Node;
+
+public class EntryEditor extends JFrame implements ActionListener {
+
+	private Entry mEntry;
+	private Node mParentNode;
+	
+	private JTextField mKeyField;
+	private JTextField mValueField;
+	private JButton mCancelButton;
+	private JButton mSaveButton;
+	
+	private ArrayList<ChangeListener> mEntryChangeListener;
+	
+	private int mEditorMode;
+	private static final int MODE_NEW=1;
+	private static final int MODE_EDIT=2;
+	
+	/*
+	 * bei MODE_EDIT:
+	 * 		entry values gleich am anfang setzen?
+	 * 		bei cancel einfach nichts tun
+	 * 		bei save einfach in object schreiben (und vllt changelistener?)
+	 * 
+	 * bei MODE_NEW:
+	 * 		bei cancel entry von parent loeschen
+	 * 		bei save entry zu parent hinzufuegen
+	 * 
+	 * 
+	 * zwei konstruktoren?
+	 * 
+	 */
+	
+	public EntryEditor() {		
+		setTitle("Entry Editor");
+		setSize(400, 150);
+
+		setLayout(new GridBagLayout());
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		JComponent con = (JComponent) getContentPane();
+		con.setBorder(new TitledBorder("Entry"));
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		JLabel keyLabel = new JLabel("Key");
+		add(keyLabel, c);
+		
+		c.gridx = 1;
+		c.gridy = 0;
+		JLabel valueLabel = new JLabel("Value");
+		add(valueLabel, c);
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		mKeyField = new JTextField(15);
+		add(mKeyField, c);
+		
+		c.gridx = 1;
+		c.gridy = 1;
+		mValueField = new JTextField(15);
+		add(mValueField, c);
+		
+		
+		c.gridx = 0;
+		c.gridy = 2;
+		mCancelButton = new JButton("Cancel");
+		mCancelButton.addActionListener(this);
+		add(mCancelButton, c);
+		
+		c.gridx = 1;
+		c.gridy = 2;
+		mSaveButton = new JButton("Save");
+		mSaveButton.addActionListener(this);
+		add(mSaveButton, c);
+	}
+
+	
+	public void showForNew(Node parent){
+		mEditorMode = MODE_NEW;
+		mParentNode = parent;
+		setEntry(new Entry(parent, "", ""));
+		
+		setVisible(true);
+	}
+	
+	public void showForEdit(Entry entry){
+		mEditorMode = MODE_EDIT;
+		setEntry(entry);
+		
+		setVisible(true);
+	}
+	
+	public Entry getEntry() {
+		return mEntry;
+	}
+
+	public void setEntry(Entry mEntry) {
+		if(mEntry!=null){
+			this.mEntry = mEntry;
+			mKeyField.setText(mEntry.getKey());
+			mValueField.setText(mEntry.getValue());
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == mCancelButton){
+			setVisible(false);
+		}
+		else if(e.getSource() == mSaveButton){
+			// TODO: change listener?
+			if(MODE_NEW == mEditorMode){
+				mEntry.setKey(mKeyField.getText());
+				mEntry.setValue(mValueField.getText());
+				mParentNode.addEntry(mEntry);
+				
+				setVisible(false);
+			}
+			else if(MODE_EDIT == mEditorMode){
+				mEntry.setKey(mKeyField.getText());
+				mEntry.setValue(mValueField.getText());
+				
+				setVisible(false);
+			}
+		}
+		
+	}
+	
+}
