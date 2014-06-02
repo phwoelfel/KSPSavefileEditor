86d45392f0e06edc171cb04973cc41f7634435e7
Philip Woelfel <philip@woelfel.at>
2014-06-02 13:27:45 +0200

    removed edit menu added about menu added icons to menus added icons to tree made tree nodes editable added right click functionality implemented changelistener to update tree

--- src/at/woelfel/philip/kspsavefileeditor/gui/MainGui.java
+++ src/at/woelfel/philip/kspsavefileeditor/gui/MainGui.java
@@ -7,25 +7,32 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
 
+import at.woelfel.philip.kspsavefileeditor.backend.ChangeListener;
 import at.woelfel.philip.kspsavefileeditor.backend.Entry;
 import at.woelfel.philip.kspsavefileeditor.backend.Node;
 import at.woelfel.philip.kspsavefileeditor.backend.NodeTableModel;
 import at.woelfel.philip.kspsavefileeditor.backend.NodeTreeModel;
 
-public class MainGui extends JFrame implements TreeSelectionListener, ActionListener{
+public class MainGui extends JFrame implements TreeSelectionListener, ActionListener, ChangeListener{
 	
 	private JTable mEntryTable;
 	private JTree mNodeTree;
@@ -33,16 +40,19 @@ public class MainGui extends JFrame implements TreeSelectionListener, ActionList
 	private JMenuItem mFileOpenItem;
 	private JMenuItem mFileSaveItem;
 	
-	private JMenuItem mEditNodeNewItem;
-	private JMenuItem mEditNodeEditItem;
-	private JMenuItem mEditNodeDeleteItem;
+	private JMenuItem mAboutInfoItem;
 	
-	private JMenuItem mEditEntryNewItem;
-	private JMenuItem mEditEntryEditItem;
-	private JMenuItem mEditEntryDeleteItem;
+	private JPopupMenu mRCPopup; // Right Click Popup
+	private JMenuItem mRCNewNodeMenu;
+	private JMenuItem mRCNewEntryMenu;
+	private JMenuItem mRCEditMenu;
+	private JMenuItem mRCDeleteMenu;
 	
 	private NodeTreeModel mNodeTreeModel;
 	private NodeTableModel mNodeTableModel;
+	
+	private EntryEditor mEntryEditor;
+	private NodeEditor mNodeEditor;
 	
 	private Node mRootNode;
 	
@@ -77,26 +87,13 @@ public class MainGui extends JFrame implements TreeSelectionListener, ActionList
 		// TODO: menu images
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
-		mFileOpenItem = initializeMenuItem(fileMenu, "Open...", this);
-		mFileSaveItem = initializeMenuItem(fileMenu, "Save...", this);
+		mFileOpenItem = initializeMenuItem(fileMenu, "Open...", this, new ImageIcon("img/load.png"));
+		mFileSaveItem = initializeMenuItem(fileMenu, "Save...", this, new ImageIcon("img/save.png"));
 		menuBar.add(fileMenu);
 		
-		JMenu editMenu = new JMenu("Edit");
-		
-		JMenu editNodeMenu = new JMenu("Node");
-		mEditNodeNewItem = initializeMenuItem(editNodeMenu, "New", this);
-		mEditNodeEditItem = initializeMenuItem(editNodeMenu, "Edit", this);
-		mEditNodeDeleteItem = initializeMenuItem(editNodeMenu, "Delete", this);
-		
-		JMenu editEntryMenu = new JMenu("Entry");
-		mEditEntryNewItem = initializeMenuItem(editEntryMenu, "New", this);
-		mEditEntryEditItem = initializeMenuItem(editEntryMenu, "Edit", this);
-		mEditEntryDeleteItem = initializeMenuItem(editEntryMenu, "Delete", this);
-		
-		editMenu.add(editNodeMenu);
-		editMenu.add(editEntryMenu);
-		menuBar.add(editMenu);
-		
+		JMenu aboutMenu = new JMenu("About");
+		mAboutInfoItem = initializeMenuItem(aboutMenu, "Info", this, new ImageIcon("img/info.png"));
+		menuBar.add(aboutMenu);
 		setJMenuBar(menuBar);
 		
 		
@@ -114,11 +111,32 @@ public class MainGui extends JFrame implements TreeSelectionListener, ActionList
 		mNodeTreeModel = new NodeTreeModel(mRootNode);
 		mNodeTree = new JTree(mNodeTreeModel);
 		mNodeTree.addTreeSelectionListener(this);
+		mNodeTree.setEditable(true);
+		mNodeTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		
+		ImageIcon rocket = new ImageIcon("img/rocket.png");
+		ImageIcon rocketFly = new ImageIcon("img/rocket-fly.png");
+	    DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
+	    //renderer.setLeafIcon(rocket);
+	    renderer.setClosedIcon(rocket);
+	    renderer.setOpenIcon(rocketFly);
+	    mNodeTree.setCellRenderer(renderer);
+	    mNodeTree.setCellEditor(new NonLeafTreeEditor(mNodeTree, renderer));
+	    
+		
+		
 		JScrollPane nodeTreeJSP = new JScrollPane(mNodeTree);
 		add(nodeTreeJSP,c);
 		
+		
+		
 		createPopupMenu();
 		
+		mEntryEditor = new EntryEditor();
+		mEntryEditor.addChangeListener(this);
+		
+		mNodeEditor = new NodeEditor();
+		mNodeEditor.addChangeListener(this);
 		
 		c.gridx = 0;
 		c.gridy = 1;
@@ -141,8 +159,15 @@ public class MainGui extends JFrame implements TreeSelectionListener, ActionList
 	}
 	
 	
-	private JMenuItem initializeMenuItem(JMenu parent, String text, ActionListener al) {
+	private JMenuItem initializeMenuItem(JComponent parent, String text, ActionListener al) {
 		JMenuItem item = new JMenuItem(text);
+		item.addActionListener(al);
+		parent.add(item);
+		return item;
+	}
+	
+	private JMenuItem initializeMenuItem(JComponent parent, String text, ActionListener al, ImageIcon img) {
+		JMenuItem item = new JMenuItem(text, img);
 		item.addActionListener(al);
 		parent.add(item);
 		return item;
@@ -150,30 +175,103 @@ public class MainGui extends JFrame implements TreeSelectionListener, ActionList
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO: actionlistener for menu items
+		
 		if (e.getSource() instanceof JMenuItem) {
-			JMenuItem source = (JMenuItem) (e.getSource());
-			String s = "Action event detected.\n" + "\tEvent source: " + source.getText() + " (an instance of " + source.getClass().getSimpleName() + ")";
-			System.out.println(s);
+			menuClick(e);
 		}
 	}
 	
-	public void createPopupMenu() {
-        JMenuItem menuItem;
- 
-        //Create the popup menu.
-        JPopupMenu popup = new JPopupMenu();
-        menuItem = new JMenuItem("Edit");
-        menuItem.addActionListener(this);
-        popup.add(menuItem);
-        menuItem = new JMenuItem("Delete");
-        menuItem.addActionListener(this);
-        popup.add(menuItem);
- 
-        //Add listener to the text area so the popup menu can come up.
-        MouseListener popupListener = new PopupListener(popup);
-        mNodeTree.addMouseListener(popupListener);
-    }
+	private void menuClick(ActionEvent e){
+		// TODO: actionlistener for menu items
+		JMenuItem source = (JMenuItem) (e.getSource());
+		String s = "Action event detected.\n" + "\tEvent source: " + source.getText() + " (an instance of " + source.getClass().getSimpleName() + ")";
+		System.out.println(s);
+		
+		
+		// TODO: move right click to function
+		if(source == mRCNewNodeMenu){
+			// get selected element
+			TreePath path = mNodeTree.getSelectionPath(); 
+			Object selection = path.getLastPathComponent();
+			// create new node and add it with selection as parent
+			Node parent=null;
+			if(selection instanceof Entry){
+				parent = ((Entry)selection).getParentNode();
+			}
+			else if(selection instanceof Node){
+				parent = (Node)selection;
+			}
+			mNodeEditor.showForNew(parent);
+		}
+		else if(source == mRCNewEntryMenu){
+			// get selected element
+			TreePath path = mNodeTree.getSelectionPath(); 
+			Object selection = path.getLastPathComponent();
+			// create new entry and add it with selection as parent
+			Node parent=null;
+			if(selection instanceof Entry){
+				parent = ((Entry)selection).getParentNode();
+			}
+			else if(selection instanceof Node){
+				parent = (Node)selection;
+			}
+			mEntryEditor.showForNew(parent);
+		}
+		else if(source == mRCEditMenu){
+			// get selected element
+			TreePath path = mNodeTree.getSelectionPath(); 
+			Object selection = path.getLastPathComponent();
+			if(selection instanceof Entry){
+				// edit Entry
+				mEntryEditor.showForEdit((Entry)selection);
+			}
+			else if(selection instanceof Node){
+				// edit Node
+				mNodeEditor.showForEdit((Node)selection);
+			}
+		}
+		else if(source == mRCDeleteMenu){
+			// get selected element
+			TreePath path = mNodeTree.getSelectionPath(); 
+			Object selection = path.getLastPathComponent();
+			int yesno = JOptionPane.showConfirmDialog(this, "Do you really want to delete this Node/Entry?", "Are you sure?", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+			if(yesno == JOptionPane.YES_OPTION){
+				int pc = path.getPathCount();
+				if(pc>=2){ // we got at least two nodes --> remove from parent
+					Node parent = (Node) path.getPathComponent(pc-2);
+					if(selection instanceof Entry){
+						parent.removeEntry((Entry)selection);
+						onEntryRemoved((Entry)selection);
+						selection = null;
+					}
+					else if(selection instanceof Node){
+						parent.removeSubNode((Node)selection);
+						onNodeRemoved((Node)selection);
+						selection = null;
+					}
+				}
+				else{ // we are deleting the root node!
+					
+				}
+			}
+		}
+		else if(source == mAboutInfoItem){
+			new AboutWindow();
+		}
+	}
+	
+	private void createPopupMenu() {
+		mRCPopup = new JPopupMenu();
+		JMenu rcNewMenu = new JMenu("New");
+		mRCNewNodeMenu = initializeMenuItem(rcNewMenu, "Node", this);
+		mRCNewEntryMenu = initializeMenuItem(rcNewMenu, "Entry", this);
+		mRCPopup.add(rcNewMenu);
+		mRCEditMenu = initializeMenuItem(mRCPopup, "Edit", this);
+		mRCDeleteMenu = initializeMenuItem(mRCPopup, "Delete", this);
+		
+		MouseListener popupListener = new PopupListener(mRCPopup);
+		mNodeTree.addMouseListener(popupListener);
+	}
 	
 	class PopupListener extends MouseAdapter {
 		JPopupMenu popup;
@@ -183,14 +281,14 @@ public class MainGui extends JFrame implements TreeSelectionListener, ActionList
 		}
 
 		public void mousePressed(MouseEvent e) {
-			maybeShowPopup(e);
+			showPopup(e);
 		}
 
 		public void mouseReleased(MouseEvent e) {
-			maybeShowPopup(e);
+			showPopup(e);
 		}
 
-		private void maybeShowPopup(MouseEvent e) {
+		private void showPopup(MouseEvent e) {
 			if (e.isPopupTrigger()) {
 				int x = e.getX();
 				int y = e.getY();
@@ -199,20 +297,47 @@ public class MainGui extends JFrame implements TreeSelectionListener, ActionList
 				if (path == null)
 					return;	
 				
+				if(path.getLastPathComponent() instanceof Node){
+					Node n = (Node)path.getLastPathComponent();
+					ArrayList<Object> nl = new ArrayList<Object>();
+					n.getPathToRoot(nl);
+					System.out.println(nl);
+				}
+				
 				tree.setSelectionPath(path);
-				Object lPC = path.getLastPathComponent();
-				if(lPC instanceof Node){
-					Node node = (Node) lPC;
-					System.out.println("right click node");
-				}
-				if(lPC instanceof Entry){
-					Entry entry = (Entry) lPC;
-					System.out.println("right click entry");
-				}
-
 				popup.show(e.getComponent(), e.getX(), e.getY());
 			}
 		}
+	}
+
+	@Override
+	public void onEntryChanged(Entry e) {
+		mNodeTreeModel.fireTreeDataChanged(e);
+	}
+
+	@Override
+	public void onNodeChanged(Node n) {
+		mNodeTreeModel.fireTreeDataChanged(n);
+	}
+
+	@Override
+	public void onEntryAdded(Entry e) {
+		mNodeTreeModel.fireTreeStructureChanged(e);
+	}
+
+	@Override
+	public void onNodeAdded(Node n) {
+		mNodeTreeModel.fireTreeStructureChanged(n);
+	}
+
+	@Override
+	public void onEntryRemoved(Entry e) {
+		mNodeTreeModel.fireTreeStructureChanged(e);
+	}
+
+	@Override
+	public void onNodeRemoved(Node n) {
+		mNodeTreeModel.fireTreeStructureChanged(n);
 	}
 
 }
---
+++ src/at/woelfel/philip/kspsavefileeditor/gui/NonLeafTreeEditor.java
@@ -0,0 +1,31 @@
+package at.woelfel.philip.kspsavefileeditor.gui;
+
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellEditor;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreePath;
+
+import at.woelfel.philip.kspsavefileeditor.backend.Node;
+
+class NonLeafTreeEditor extends DefaultTreeCellEditor {
+	public NonLeafTreeEditor(JTree tree, DefaultTreeCellRenderer renderer) {
+		super(tree, renderer);
+	}
+
+	@Override
+	public boolean isCellEditable(EventObject event) {
+		if (!super.isCellEditable(event)) {
+			return false;
+		}
+		if (event != null && event.getSource() instanceof JTree && event instanceof MouseEvent) {
+			MouseEvent mouseEvent = (MouseEvent) event;
+			JTree tree = (JTree) event.getSource();
+			TreePath path = tree.getPathForLocation(mouseEvent.getX(), mouseEvent.getY());
+			return path.getLastPathComponent() instanceof Node; // root and direct children are not editable
+		}
+		return false;
+	}
+}
